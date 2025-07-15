51e751b7ada366ebd6154a38f31e8352720dec17
zen1th <2251829764@qq.com>
2025-07-15 17:00:18 +0800

    Site updated: 2025-07-15 17:00:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/13/hello-world/"];function toRandomPost(){
+var posts=["2023/07/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
